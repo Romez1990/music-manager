c3948ab7bdaa80ceff6befe17e9a168f53ee392f
Romez1990 <Romez19900@gmail.com>
2019-06-30 15:56:42 +0300

    Create Writer class for write ID3 tags to audio files

---
+++ lyrics/writer.py
@@ -0,0 +1,24 @@
+import eyed3
+
+
+class Song:
+    def __init__(self, path):
+        self.song = eyed3.load(path)
+    
+    def __del__(self):
+        self.song.tag.save()
+    
+    def artist(self):
+        return self.song.tag.artist
+    
+    def title(self):
+        return self.song.tag.title
+    
+    def write_lyrics(self, lyrics):
+        lyrics = Song.process_lyrics(lyrics)
+        self.song.tag.lyrics.set(lyrics)
+    
+    @staticmethod
+    def process_lyrics(text):
+        text = text.replace('’', '\'')
+        return text
