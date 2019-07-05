749a21333694f93884cb5955e24bd22e61cc897f
Romez1990 <Romez19900@gmail.com>
2019-07-05 08:53:13 +0300

    Change writer engine from eyed3 to mutagen

--- lyrics/writer.py
+++ lyrics/writer.py
@@ -1,23 +1,24 @@
-import eyed3
+from mutagen.id3 import ID3, USLT
 
 
 class Song:
     def __init__(self, path):
-        self.song = eyed3.load(path)
+        self.path = path
+        self.tags = ID3(path)
     
     def __del__(self):
-        self.song.tag.save()
+        self.tags.save(self.path)
     
     def artist(self):
-        return self.song.tag.artist
+        return self.tags['TPE1'].text[0]
     
     def title(self):
-        return self.song.tag.title
+        return self.tags['TIT2'].text[0]
     
     def write_lyrics(self, lyrics):
         if lyrics is not None:
             lyrics = Song.process_lyrics(lyrics)
-            self.song.tag.lyrics.set(lyrics)
+            self.tags['USLT'] = USLT(encoding=3, text=lyrics)
     
     @staticmethod
     def process_lyrics(text):
