26d68868c2774734695e43f2c0da1b8ea156b2d7
Romez1990 <Romez19900@gmail.com>
2019-07-06 21:48:41 +0300

    Rearrange SettingsWindow class

--- windows/settings_window.py
+++ windows/settings_window.py
@@ -8,34 +8,21 @@ class SettingsWindow(QDialog, Ui_settings_window):
     def __init__(self, parent=None, *args, **kwargs):
         QDialog.__init__(self)
         self.setupUi(self)
-        
         self.config = Config()
+        self.set_options_from_config()
+        self.connect_signals()
+        self.errors = {}
+    
+    def set_options_from_config(self):
         self.driver_line_edit.setText(self.config.get('browser', 'driver'))
         self.minimize_check_box.setChecked(self.config.getboolean('browser', 'minimize'))
         self.api_token_line_edit.setText(self.config.get('genius', 'api_token'))
-        
+    
+    def connect_signals(self):
         self.driver_line_edit.textChanged.connect(self.check_driver_existence)
         self.driver_browse_push_button.clicked.connect(self.browse_driver)
-        
-        def set_driver():
-            self.config.set_option('browser', 'driver', self.driver_line_edit.text())
-        
-        self.driver_line_edit.editingFinished.connect(set_driver)
-        
-        def set_minimize():
-            self.config.set_option('browser', 'minimize', self.minimize_check_box.isChecked())
-        
-        self.minimize_check_box.stateChanged.connect(set_minimize)
-        
-        def set_api_token():
-            self.config.set_option('genius', 'api_token', self.api_token_line_edit.text())
-        
-        self.api_token_line_edit.editingFinished.connect(set_api_token)
-        
         self.ok_push_button.clicked.connect(self.save_and_close)
         self.cancel_push_button.clicked.connect(self.close)
-        
-        self.errors = {}
     
     def check_driver_existence(self):
         path = self.driver_line_edit.text()
@@ -54,5 +41,8 @@ class SettingsWindow(QDialog, Ui_settings_window):
         self.ok_push_button.setEnabled(not there_are_errors)
     
     def save_and_close(self):
+        self.config.set_option('browser', 'driver', self.driver_line_edit.text())
+        self.config.set_option('browser', 'minimize', self.minimize_check_box.isChecked())
+        self.config.set_option('genius', 'api_token', self.api_token_line_edit.text())
         self.config.save()
         self.close()
