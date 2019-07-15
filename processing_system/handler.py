9b4639fdfdf77504333d5ba5820a52b40f5b1726
Romez1990 <Romez19900@gmail.com>
2019-07-15 16:38:36 +0300

    Make some methods private

--- processing_system/handler.py
+++ processing_system/handler.py
@@ -5,14 +5,14 @@ class MainHandler:
     def __init__(self, error_handlers):
         self.error_handlers = error_handlers
         self.handlers = {
-            Flag.lyrics: self.handle_lyrics
+            Flag.lyrics: self.__handle_lyrics
         }
     
     def handle(self, flags, mode, filesystem):
         for flag in flags:
             self.handlers[flag](mode, filesystem)
     
-    def handle_lyrics(self, mode, filesystem):
+    def __handle_lyrics(self, mode, filesystem):
         from lyrics.handler import Handler
         handler = Handler(self.error_handlers)
         handler.handle(filesystem, mode)
--- processing_system/processor.py
+++ processing_system/processor.py
@@ -10,33 +10,33 @@ class Processor:
         self.on_complete = None
         
         self.filters = {
-            Mode.album:       self.filter_album,
-            Mode.band:        self.filter_band,
-            Mode.compilation: self.filter_compilation,
+            Mode.album:       self.__filter_album,
+            Mode.band:        self.__filter_band,
+            Mode.compilation: self.__filter_compilation,
         }
     
     def process(self, dir, mode, flags):
         self.filesystem = [] if mode is Mode.album else {}
-        self.iterate(dir, mode, self.filesystem)
+        self.__iterate(dir, mode, self.filesystem)
         handler = MainHandler(self.error_handlers)
         handler.handle(flags, mode, self.filesystem)
         self.on_complete()
     
-    def iterate(self, dir, mode, container):
+    def __iterate(self, dir, mode, container):
         for filename in sorted(os.listdir(dir)):
             path = os.path.join(dir, filename)
             self.filters[mode](path, container)
     
-    def filter_album(self, path, container):
+    def __filter_album(self, path, container):
         if path.endswith('.mp3') and os.path.isfile(path):
             container.append(path)
     
-    def filter_band(self, path, container):
+    def __filter_band(self, path, container):
         if os.path.isdir(path):
             container[path] = []
-            self.iterate(path, Mode(Mode.album), container[path])
+            self.__iterate(path, Mode(Mode.album), container[path])
     
-    def filter_compilation(self, path, container):
+    def __filter_compilation(self, path, container):
         if os.path.isdir(path):
             container[path] = {}
-            self.iterate(path, Mode(Mode.band), container[path])
+            self.__iterate(path, Mode(Mode.band), container[path])
