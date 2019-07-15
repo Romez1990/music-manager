3b431a634e1e55e57526f32c53e086d27e3cc359
Romez1990 <Romez19900@gmail.com>
2019-07-15 18:36:09 +0300

    Rearrange main handler code

--- processing_system/handler.py
+++ processing_system/handler.py
@@ -4,15 +4,20 @@ from processing_system.flags import Flag
 class MainHandler:
     def __init__(self, error_handlers):
         self.error_handlers = error_handlers
-        self.handlers = {
-            Flag.lyrics: self.__handle_lyrics
+        self.handlers = {}
+    
+    def create_handlers(self, flags):
+        creates = {
+            Flag.lyrics: self.__create_lyrics_handler,
         }
+        for flag in flags:
+            self.handlers[flag] = creates[flag]()
+    
+    def __create_lyrics_handler(self):
+        from lyrics.handler import Handler
+        return Handler(self.error_handlers)
     
     def handle(self, flags, mode, filesystem):
+        self.create_handlers(flags)
         for flag in flags:
-            self.handlers[flag](mode, filesystem)
-    
-    def __handle_lyrics(self, mode, filesystem):
-        from lyrics.handler import Handler
-        handler = Handler(self.error_handlers)
-        handler.handle(filesystem, mode)
+            self.handlers[flag].handle(mode, filesystem)
