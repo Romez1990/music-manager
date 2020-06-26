854226c689ce1693b1e2f93fb7a3c55ea7feeb2a
Romez <Romez1990@gmail.com>
2020-06-26 10:37:26 +0300

    feat(Core.FileSystem): create fs info factory

---
+++ src/Core/FileSystem/IFsInfoFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.IO.Abstractions;
+
+namespace Core.FileSystem
+{
+    public interface IFsInfoFactory
+    {
+        IDirectoryInfo CreateDirectoryInfo(string path);
+
+        IFileInfo CreateFileInfo(string path);
+    }
+}
