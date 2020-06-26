76b8e6ec2dece555a6d1272a14fc774eb0fd4cd2
Romez <Romez1990@gmail.com>
2020-06-26 14:46:30 +0300

    feat(Core.FileSystem): create fs node element base

---
+++ src/Core/FileSystem/FsNodeElementCheckEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Core.FileSystem
+{
+    public class FsNodeElementCheckEventArgs : EventArgs
+    {
+        public FsNodeElementCheckEventArgs(CheckState checkState)
+        {
+            CheckState = checkState;
+        }
+
+        public CheckState CheckState { get; }
+    }
+}
