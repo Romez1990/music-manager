83ddb38307beed48472643cc31bb1f029f3ae094
Romez <Romez1990@gmail.com>
2020-06-26 10:33:57 +0300

    feat(Core.Test.FileSystem): create test files loader

---
+++ test/Core.Test/FileSystem/TestFilesLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Core.Test.Utils;
+
+namespace Core.Test.FileSystem
+{
+    public class TestFilesLoader
+    {
+        public TestFilesLoader()
+        {
+            var filesHelper = new FsHelper();
+            var solutionDirectory = filesHelper.GetSolutionDirectory();
+            TestFilesDirectoryPath = Path.Combine(solutionDirectory.FullName, "test", "test_compilation");
+            _testFilesDirectoryInfo = new DirectoryInfo(TestFilesDirectoryPath);
+        }
+
+        public string TestFilesDirectoryPath { get; }
+
+        private readonly DirectoryInfo _testFilesDirectoryInfo;
+
+        public Dictionary<DirectoryInfo, Dictionary<DirectoryInfo, FileInfo[]>> GetFiles()
+        {
+            return _testFilesDirectoryInfo
+                .GetDirectories()
+                .ToDictionary(
+                    bandDirectoryInfo => bandDirectoryInfo,
+                    bandDirectoryInfo => bandDirectoryInfo
+                        .GetDirectories()
+                        .ToDictionary(
+                            albumDirectoryInfo => albumDirectoryInfo,
+                            albumDirectoryInfo => albumDirectoryInfo
+                                .GetFiles()
+                        )
+                );
+        }
+    }
+}
