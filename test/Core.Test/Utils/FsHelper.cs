f7734e4ca43ac9c5614710d7d28cc712c8bb3fb8
Romez <Romez1990@gmail.com>
2020-06-26 10:19:15 +0300

    feat(Core.Test.Utils): create fs helper

---
+++ test/Core.Test/Utils/FsHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Core.Test.Utils
+{
+    public class FsHelper
+    {
+        public DirectoryInfo GetSolutionDirectory()
+        {
+            var assemblyLocation = Assembly.GetExecutingAssembly().Location;
+            var assemblyDirectoryInfo = Directory.GetParent(assemblyLocation);
+            return GetSolutionDirectoryHelper(assemblyDirectoryInfo);
+        }
+
+        private DirectoryInfo GetSolutionDirectoryHelper(DirectoryInfo directoryInfo)
+        {
+            if (directoryInfo.GetFiles("*.sln").Length != 0)
+                return directoryInfo;
+
+            var parent = directoryInfo.Parent;
+            if (parent == null)
+                throw new NullReferenceException("Put assembly inside the solution directory");
+            return GetSolutionDirectoryHelper(parent);
+        }
+    }
+}
