0d69af013181150508d73cfcd397094300feb5de
Romez <Romez1990@gmail.com>
2020-06-26 14:46:30 +0300

    feat(Core.FileSystem): create fs node element base

---
+++ src/Core/FileSystem/IFsNodeElement.cs
@@ -0,0 +1,13 @@
+namespace Core.FileSystem
+{
+    public interface IFsNodeElement<out T> where T : IFsNode
+    {
+        T FsNode { get; }
+
+        CheckState CheckState { get; }
+
+        void Uncheck();
+  
+        void Check();
+    }
+}
