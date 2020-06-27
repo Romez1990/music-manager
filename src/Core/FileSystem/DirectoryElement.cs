e6cabaadf729a7f19d53b58cf808f92fe69076aa
Romez <Romez1990@gmail.com>
2020-06-27 11:23:34 +0300

    feat(Core.FileSystem): create directory element

---
+++ src/Core/FileSystem/DirectoryElement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace Core.FileSystem
+{
+    public class DirectoryElement : FsNodeElementBase<IDirectory>, IDirectoryElement
+    {
+        public DirectoryElement(IDirectory directory, EventHandler<FsNodeElementCheckEventArgs> uncheckHandler,
+            EventHandler<FsNodeElementCheckEventArgs> checkHandler) : base(directory, uncheckHandler, checkHandler)
+        {
+        }
+
+        public DirectoryElement(IDirectory directory) : base(directory)
+        {
+        }
+
+        public ImmutableArray<IFsNodeElement<IFsNode>> Content { get; }
+    }
+}
---
+++ src/Core/FileSystem/IDirectoryElement.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Immutable;
+
+namespace Core.FileSystem
+{
+    public interface IDirectoryElement : IFsNodeElement<IDirectory>
+    {
+        ImmutableArray<IFsNodeElement<IFsNode>> Content { get; }
+    }
+}
