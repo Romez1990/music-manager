899ecfdc4e1391010c5743cdfba1613df78313da
Romez <Romez1990@gmail.com>
2020-06-27 12:02:30 +0300

    feat(Core.FileSystem): implement directory content

--- src/Core/FileSystem/Directory.cs
+++ src/Core/FileSystem/Directory.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections.Immutable;
 using System.IO;
 using System.IO.Abstractions;
+using System.Linq;
 
 namespace Core.FileSystem
 {
     public class Directory : FsNodeBase<IDirectoryInfo>, IDirectory
     {
-        public Directory(IDirectoryInfo info) : base(info)
+        public Directory(IFsNodeFactory fsNodeFactory, IDirectoryInfo info) : base(info)
         {
+            _fsNodeFactory = fsNodeFactory;
+            Content = ReadContent();
         }
 
+        private readonly IFsNodeFactory _fsNodeFactory;
+
         public ImmutableArray<IFsNode> Content { get; }
+
+        private ImmutableArray<IFsNode> ReadContent()
+        {
+            return Info
+                .GetDirectories()
+                .Select(info => _fsNodeFactory.InstantiateDirectory(info))
+                .Cast<IFsNode>()
+                .Concat(Info
+                    .GetFiles()
+                    .Select(info => _fsNodeFactory.InstantiateFile(info)))
+                .ToImmutableArray();
+        }
 
         public override void Rename(string newName)
         {
