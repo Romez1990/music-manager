c4233b8e4d3166d0087b7b418271b964b619d9e2
Romez <Romez1990@gmail.com>
2020-06-27 11:23:34 +0300

    feat(Core.FileSystem): create directory element

---
+++ src/Core/FileSystem/DirectoryElement.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Core.FileSystem
+{
+    public class DirectoryElement : FsNodeElementBase<IDirectory>, IDirectoryElement
+    {
+        public DirectoryElement(IDirectory directory, EventHandler<FsNodeElementCheckEventArgs> checkHandler,
+            EventHandler<FsNodeElementCheckEventArgs> checkPartiallyHandler,
+            EventHandler<FsNodeElementCheckEventArgs> uncheckHandler) : base(directory, checkHandler, uncheckHandler)
+        {
+            CheckedPartiallyEvent += checkPartiallyHandler;
+        }
+
+        public DirectoryElement(IDirectory directory) : base(directory)
+        {
+        }
+
+        public ImmutableArray<IFsNodeElement<IFsNode>> Content { get; }
+
+        private event EventHandler<FsNodeElementCheckEventArgs> CheckedPartiallyEvent;
+
+        private void OnCheckedPartiallyEvent()
+        {
+            CheckedPartiallyEvent?.Invoke(this, new FsNodeElementCheckEventArgs(CheckState));
+        }
+
+        public override void Check()
+        {
+            base.Check();
+            Content.ToList().ForEach(fsNodeElement => fsNodeElement.Check());
+        }
+
+        public override void Uncheck()
+        {
+            base.Check();
+            Content.ToList().ForEach(fsNodeElement => fsNodeElement.Uncheck());
+        }
+
+        private void ContentCheckHandler(object sender, FsNodeElementCheckEventArgs e)
+        {
+            if (Content.All(fsNodeElement => fsNodeElement.CheckState == CheckState.Checked))
+            {
+                CheckState = CheckState.Checked;
+                OnCheckEvent();
+            }
+            else if (CheckState == CheckState.Unchecked)
+            {
+                CheckState = CheckState.CheckedPartially;
+                OnCheckedPartiallyEvent();
+            }
+        }
+
+        private void ContentCheckPartiallyHandler(object sender, FsNodeElementCheckEventArgs e)
+        {
+            if (CheckState != CheckState.CheckedPartially)
+            {
+                CheckState = CheckState.CheckedPartially;
+                OnCheckedPartiallyEvent();
+            }
+        }
+
+        private void ContentUncheckHandler(object sender, FsNodeElementCheckEventArgs e)
+        {
+            if (Content.All(fsNodeElement => fsNodeElement.CheckState == CheckState.Unchecked))
+            {
+                CheckState = CheckState.Unchecked;
+                OnUncheckEvent();
+            }
+            else if (CheckState != CheckState.CheckedPartially)
+            {
+                CheckState = CheckState.CheckedPartially;
+                OnCheckedPartiallyEvent();
+            }
+        }
+    }
+}
