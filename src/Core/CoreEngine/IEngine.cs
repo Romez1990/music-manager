a93d7e19660fa1b327a5c15de840c1f3c6a5a9ac
Romez <Romez1990@gmail.com>
2020-06-28 08:17:25 +0300

    feat(Core.CoreEngine): create engine

---
+++ src/Core/CoreEngine/IEngine.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Core.FileSystem;
+using Core.Operation;
+
+namespace Core.CoreEngine
+{
+    public interface IEngine
+    {
+        IDirectoryElement DirectoryElement { get; }
+        bool SetDirectory(string path);
+        void Scan(Mode mode);
+        void PerformActions(IEnumerable<IOperation> operations);
+    }
+}
