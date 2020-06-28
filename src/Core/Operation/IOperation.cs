ab847621302d4faca41ff5e8eace7ebf7af3d5a0
Romez <Romez1990@gmail.com>
2020-06-28 08:16:22 +0300

    feat(Core.Operation): create operation interface

---
+++ src/Core/Operation/IOperation.cs
@@ -0,0 +1,10 @@
+﻿using Core.CoreEngine;
+using Core.FileSystem;
+
+namespace Core.Operation
+{
+    public interface IOperation
+    {
+        void Perform(IDirectoryElement directoryElement, Mode mode);
+    }
+}
