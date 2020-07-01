f75bbe8534337fa98ee5cd71b56b9d2372f04492
Romez <Romez1990@gmail.com>
2020-07-01 17:09:45 +0300

    make main explicit

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,8 +1,8 @@
 ﻿namespace ConsoleApp
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
         }
     }
