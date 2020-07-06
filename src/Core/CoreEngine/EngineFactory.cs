f2109a29d7ea168527136109f953d98781f83f14
Romez <Romez1990@gmail.com>
2020-07-06 12:16:42 +0300

    refactor(Core.CoreEngine): use operation repository in engine performer

--- src/Core/CoreEngine/EngineFactory.cs
+++ src/Core/CoreEngine/EngineFactory.cs
@@ -1,16 +1,20 @@
 using Core.FileScanner;
 using Core.FileSystem;
+using Core.Operation;
 
 namespace Core.CoreEngine
 {
     public class EngineFactory : IEngineFactory
     {
-        public EngineFactory(IScanner scanner)
+        public EngineFactory(IScanner scanner, IOperationRepository operationRepository)
         {
             _scanner = scanner;
+            _operationRepository = operationRepository;
         }
 
         private readonly IScanner _scanner;
+
+        private readonly IOperationRepository _operationRepository;
 
         public IEngineScanner CreateEngineScanner(IDirectoryElement directoryElement)
         {
@@ -19,7 +23,7 @@ namespace Core.CoreEngine
 
         public IEnginePerformer CreateEnginePerformer(IDirectoryElement directoryElement, Mode mode)
         {
-            return new EnginePerformer(directoryElement, mode);
+            return new EnginePerformer(_operationRepository, directoryElement, mode);
         }
     }
 }
