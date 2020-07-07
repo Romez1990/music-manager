8e5fb92ca57824de64fae5905f2e8743e1ee0827
Romez <Romez1990@gmail.com>
2020-07-07 07:55:27 +0300

    feat(ConsoleApp): create app module

---
+++ src/ConsoleApp/AppModule.cs
@@ -0,0 +1,11 @@
+using Autofac;
+
+namespace ConsoleApp
+{
+    public class AppModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+        }
+    }
+}
