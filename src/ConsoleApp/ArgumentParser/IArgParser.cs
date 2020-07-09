da4f8b404c34784312256291c1c92d701d9ba91a
Romez <Romez1990@gmail.com>
2020-07-09 08:48:27 +0300

    feat(ConsoleApp.ArgumentParser): create arg parser

---
+++ src/ConsoleApp/ArgumentParser/IArgParser.cs
@@ -0,0 +1,11 @@
+using System.Collections.Immutable;
+using ConsoleApp.Application;
+using LanguageExt;
+
+namespace ConsoleApp.ArgumentParser
+{
+    public interface IArgParser
+    {
+        Either<ArgumentParserException, AppOptions> Parse(ImmutableArray<string> args, AppOptionsDefault appOptionsDefault);
+    }
+}
