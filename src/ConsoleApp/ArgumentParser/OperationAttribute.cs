bf3ec233b1c0325a6c26f385d1666345bbc4c5c0
Romez <Romez1990@gmail.com>
2020-07-12 07:57:13 +0300

    feat(ConsoleApp.ArgumentParser): create operation attribute

---
+++ src/ConsoleApp/ArgumentParser/OperationAttribute.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp.ArgumentParser
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class OperationAttribute : Attribute
+    {
+        public OperationAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
