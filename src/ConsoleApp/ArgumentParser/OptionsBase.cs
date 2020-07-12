8dc8c2c72a4dc9c0e3fa6529ee0de3ae313e4628
Romez <Romez1990@gmail.com>
2020-07-12 07:57:00 +0300

    feat(ConsoleApp.ArgumentParser): create base options

---
+++ src/ConsoleApp/ArgumentParser/OptionsBase.cs
@@ -0,0 +1,25 @@
+using CommandLine;
+
+namespace ConsoleApp.ArgumentParser
+{
+    public abstract class OptionsBase
+    {
+        [Value(0, HelpText = "Directory path")]
+        public string Path { get; set; }
+
+        [Option('a', "album", SetName = "album", HelpText = "Album directory mode")]
+        public bool Album { get; set; }
+
+        [Option('b', "band", SetName = "band", HelpText = "Band directory mode")]
+        public bool Band { get; set; }
+
+        [Option('c', "compilation", SetName = "compilation", HelpText = "Compilation directory mode")]
+        public bool Compilation { get; set; }
+
+        [Option('r', "rename", HelpText = "Rename tracks and folders")]
+        public bool Rename { get; set; }
+
+        [Option('l', "lyrics", HelpText = "Add lyrics to tracks")]
+        public bool Lyrics { get; set; }
+    }
+}
