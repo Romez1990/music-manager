8be0cc6032e2583ff8be3130cf1c254787ae5b80
Romez <Romez1990@gmail.com>
2020-11-19 19:59:27 +0300

    feat(Core.Serializers): create json serializer

---
+++ src/Core/Serializers/JsonSerializer.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Core.Serializers
+{
+    public class JsonSerializer : IJsonSerializer
+    {
+        public T Deserialize<T>(string json) =>
+            JsonConvert.DeserializeObject<T>(json);
+    }
+}
