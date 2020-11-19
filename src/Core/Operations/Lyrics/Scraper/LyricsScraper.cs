ea5b688cc797e9524d4b919ff0d9402f4069562d
Romez <Romez1990@gmail.com>
2020-11-19 19:14:09 +0300

    feat(Core.Operations.Lyrics.Scraper): add second method to find lyrics

--- src/Core/Operations/Lyrics/Scraper/LyricsScraper.cs
+++ src/Core/Operations/Lyrics/Scraper/LyricsScraper.cs
@@ -26,9 +26,28 @@ namespace Core.Operations.Lyrics.Scraper
                 .Bind(document => GetLyricsElement(file, document).ToAsync())
                 .Map(GetLyricsText);
 
-        private Either<LyricsNotFoundException, IElement> GetLyricsElement(IFileElement file, IParentNode parent) =>
-            Optional(parent.QuerySelector(".lyrics"))
+        private Either<LyricsNotFoundException, IElement> GetLyricsElement(IFileElement file, IDocument document) =>
+            GetLyricsElement1Method(document)
+                .Some(Some)
+                .None(() => GetLyricsElement2Method(document))
                 .ToEither(() => new LyricsNotFoundException(file));
+
+        private Option<IElement> GetLyricsElement1Method(IParentNode parent) =>
+            Optional(parent.QuerySelector(".lyrics"));
+
+        private Option<IElement> GetLyricsElement2Method(IDocument document) =>
+            Optional(document.QuerySelector("main > :nth-child(2) > :nth-child(3)"))
+                .Map(element =>
+                {
+                    foreach (var br in element.QuerySelectorAll("br"))
+                    {
+                        var span = document.CreateElement("span");
+                        span.TextContent = "\n";
+                        br.ReplaceWith(span);
+                    }
+
+                    return element;
+                });
 
         private string GetLyricsText(IElement lyricsBlock) =>
             lyricsBlock.TextContent;
