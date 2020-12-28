d1f97d845d425775903c7d43d3158ec157291b2b
Romez <Romez1990@gmail.com>
2020-12-28 17:18:39 +0300

    test(Core.Test.Renaming): create test for album and track renamers

---
+++ test/Core.Test/Renaming/AlbumRenamerTest.cs
@@ -0,0 +1,57 @@
+﻿using Core.Renaming;
+using NUnit.Framework;
+
+namespace Core.Test.Renaming
+{
+    [TestFixture]
+    public class AlbumRenamerTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _albumRenamer = new AlbumRenamer();
+        }
+
+        private AlbumRenamer _albumRenamer;
+
+        [Test]
+        public void RenameAlbumWithoutNumber_WithBand()
+        {
+            var result = _albumRenamer.RenameAlbumWithoutNumber("2009 - Asking Alexandria - Stand Up And Scream");
+
+            Assert.That(result, Is.EqualTo("Stand Up And Scream (2009)"));
+        }
+
+        [Test]
+        public void RenameAlbumWithoutNumber_WithoutBand()
+        {
+            var result = _albumRenamer.RenameAlbumWithoutNumber("2009 - Stand Up And Scream");
+
+            Assert.That(result, Is.EqualTo("Stand Up And Scream (2009)"));
+        }
+
+        [Test]
+        public void RenameAlbumWithNumber_1Number()
+        {
+            var result = _albumRenamer.RenameAlbumWithNumber("2009 - Asking Alexandria - Stand Up And Scream", 1, 1);
+
+            Assert.That(result, Is.EqualTo("1 Stand Up And Scream (2009)"));
+        }
+
+        [Test]
+        public void RenameAlbumWithNumber_04Number()
+        {
+            var result = _albumRenamer.RenameAlbumWithNumber("2009 - Asking Alexandria - Stand Up And Scream", 2, 4);
+
+            Assert.That(result, Is.EqualTo("04 Stand Up And Scream (2009)"));
+        }
+
+        [Test]
+        public void RenameAlbumWithNumber_13Number()
+        {
+            var result = _albumRenamer.RenameAlbumWithNumber("2009 - Asking Alexandria - Stand Up And Scream", 2, 13);
+
+            Assert.That(result, Is.EqualTo("13 Stand Up And Scream (2009)"));
+        }
+    }
+}
---
+++ test/Core.Test/Renaming/TrackRenamerTest.cs
@@ -0,0 +1,41 @@
+﻿using Core.Renaming;
+using NUnit.Framework;
+
+namespace Core.Test.Renaming
+{
+    [TestFixture]
+    public class TrackRenamerTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _trackRenamer = new TrackRenamer();
+        }
+
+        private TrackRenamer _trackRenamer;
+
+        [Test]
+        public void RenameTrack_RemovesDot()
+        {
+            var result = _trackRenamer.RenameTrack("01. Alerion.mp3", false);
+
+            Assert.That(result, Is.EqualTo("01 Alerion.mp3"));
+        }
+
+        [Test]
+        public void RenameTrack_IfNoDot_ReturnsSameName()
+        {
+            var result = _trackRenamer.RenameTrack("01 Alerion.mp3", false);
+
+            Assert.That(result, Is.EqualTo("01 Alerion.mp3"));
+        }
+
+        [Test]
+        public void RenameTrack_IfIsTrackNumberOneDigit_RemovesZero()
+        {
+            var result = _trackRenamer.RenameTrack("01 Alerion.mp3", true);
+
+            Assert.That(result, Is.EqualTo("1 Alerion.mp3"));
+        }
+    }
+}
