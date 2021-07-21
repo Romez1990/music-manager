337958d0b39d7595a6ce9fe4dac218af7e19ba07
Romez <Romez1990@gmail.com>
2021-07-21 11:16:56 +0300

    feat(Core.FileSystem): create directory exceptions

---
+++ src/Core/FileSystem/Exceptions/DirectoryAlreadyExistsException.cs
@@ -0,0 +1,5 @@
+﻿namespace Core.FileSystem.Exceptions {
+    public sealed class DirectoryAlreadyExistsException : FsNodeAlreadyExistsException {
+        public DirectoryAlreadyExistsException(string newDirectoryName) : base("directory", newDirectoryName) { }
+    }
+}
---
+++ src/Core/FileSystem/Exceptions/DirectoryIsBeingUsedException.cs
@@ -0,0 +1,5 @@
+﻿namespace Core.FileSystem.Exceptions {
+    public sealed class DirectoryIsBeingUsedException : FsNodeIsBeingUsedException {
+        public DirectoryIsBeingUsedException(string directoryName) : base("directory", directoryName) { }
+    }
+}
