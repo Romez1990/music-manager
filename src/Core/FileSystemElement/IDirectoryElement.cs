1235545f26671ad928732ad30b27c80bad07f768
Romez <Romez1990@gmail.com>
2021-07-21 11:07:29 +0300

    feat(Core.FileSystemElement): create directory element interface

---
+++ src/Core/FileSystemElement/IDirectoryElement.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Core.FileSystem.Exceptions;
+using LanguageExt;
+
+namespace Core.FileSystemElement {
+    public interface IDirectoryElement : IFsNodeElement {
+        IReadOnlyList<IFsNodeElement> Children { get; }
+        Either<FsException, IDirectoryElement> Rename(string newName);
+        IDirectoryElement Uncheck(bool ignoreIfUnchecked = false);
+        IDirectoryElement Check(bool ignoreIfChecked = false);
+        IDirectoryElement SetCheckState(CheckState checkState, bool ignoreIfSameState = false);
+        bool IsExpanded { get; }
+        IDirectoryElement Expand();
+        IDirectoryElement Collapse();
+    }
+}
