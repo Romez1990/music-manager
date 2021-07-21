7c513e2385cd293031868cb21f302d7eae58190e
Romez <Romez1990@gmail.com>
2021-07-21 12:03:15 +0300

    feat(Core.Operation): create operation interface

---
+++ src/Core/Operation/IOperation.cs
@@ -0,0 +1,10 @@
+﻿using Core.FileSystemElement;
+using Core.OperationEngine;
+
+namespace Core.Operation {
+    public interface IOperation {
+        string Name { get; }
+        string Description { get; }
+        OperationResult Perform(IDirectoryElement directory, DirectoryMode directoryMode);
+    }
+}
---
+++ src/Core/Operation/OperationResult.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Immutable;
+using Core.FileSystemElement;
+
+namespace Core.Operation {
+    public record OperationResult(IDirectoryElement Directory, ImmutableList<OperationException> Exceptions) {
+        public static OperationResult operator +(OperationResult a, OperationResult b) =>
+            new(b.Directory, a.Exceptions.AddRange(b.Exceptions));
+    }
+}
