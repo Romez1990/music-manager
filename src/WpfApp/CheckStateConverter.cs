791a02830c3d9b6f895cbc4a4249d623a1c33173
Romez <Romez1990@gmail.com>
2021-07-21 13:20:34 +0300

    temp wpf

---
+++ src/WpfApp/CheckStateConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Core.FileSystemElement;
+
+namespace WpfApp {
+    [ValueConversion(typeof(CheckState), typeof(bool?))]
+    public class CheckStateConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            (CheckState)value switch {
+                CheckState.Unchecked => false,
+                CheckState.CheckedPartially => null,
+                CheckState.Checked => true,
+                _ => throw new NotSupportedException(),
+            };
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotSupportedException();
+    }
+}
---
+++ src/WpfApp/FsNodeStyleSelector.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Core.FileSystemElement;
+
+namespace WpfApp {
+    public class FsNodeStyleSelector : StyleSelector {
+        public Style DirectoryStyle { get; set; }
+        public Style FileStyle { get; set; }
+
+        public override Style SelectStyle(object item, DependencyObject container) =>
+            ((IFileElement)item).Match(
+                _ => DirectoryStyle,
+                _ => FileStyle
+            );
+    }
+}
