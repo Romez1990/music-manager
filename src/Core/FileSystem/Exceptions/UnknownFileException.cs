cc6e4de43e7331bf645f30b0d4a56e8762d86112
Romez <Romez1990@gmail.com>
2021-07-21 11:10:51 +0300

    feat(Core.FileSystem): create file exceptions

---
+++ src/Core/FileSystem/Exceptions/UnknownFileException.cs
@@ -0,0 +1,5 @@
+﻿namespace Core.FileSystem.Exceptions {
+    public sealed class UnknownFileException : FsException {
+        public UnknownFileException(string message) : base(message) { }
+    }
+}
