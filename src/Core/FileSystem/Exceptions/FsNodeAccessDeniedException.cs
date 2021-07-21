c8dc32c37e5ca29bf18e00255144cfbe2871361f
Romez <Romez1990@gmail.com>
2021-07-21 11:11:02 +0300

    feat(Core.FileSystem): create fs node exceptions

---
+++ src/Core/FileSystem/Exceptions/FsNodeAccessDeniedException.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.FileSystem.Exceptions {
+    public abstract class FsNodeAccessDeniedException : FsException {
+        protected FsNodeAccessDeniedException(string fsNodeType, string fsNodeName)
+            : base(GetMessage(fsNodeType, fsNodeName)) { }
+
+        private static string GetMessage(string fsNodeType, string fsNodeName) =>
+            $"Access denied for {fsNodeType} '{fsNodeName}'";
+    }
+}
