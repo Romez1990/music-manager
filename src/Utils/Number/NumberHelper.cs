1e44f0bbf254969641f4588b8805e75ae5aea722
Romez <Romez1990@gmail.com>
2021-07-21 21:42:44 +0300

    feat(Utils.Number): create NumberHelper

---
+++ src/Utils/Number/NumberHelper.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Utils.Number {
+    public static class NumberHelper {
+        public static int GetLength(this int number) =>
+            (int)Math.Log10(number) + 1;
+    }
+}
