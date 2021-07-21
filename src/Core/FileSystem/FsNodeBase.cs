822eda2d5bd2aa3b5064b373f655020d5a0acb27
Romez <Romez1990@gmail.com>
2021-07-21 10:58:41 +0300

    feat(Core.FileSystem): create fs node base

---
+++ src/Core/FileSystem/FsNodeBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Core.FileSystem {
+    public abstract class FsNodeBase<T> : IFsNode where T : FileSystemInfo {
+        protected FsNodeBase(T info) {
+            Info = info;
+        }
+
+        protected T Info { get; }
+
+        public bool Exists => Info.Exists;
+        public string Name => Info.Name;
+        public string Path => Info.FullName;
+        public abstract string ParentPath { get; }
+
+        public event EventHandler<FsNodeChangedEventArgs> Changed;
+
+        protected void InvokeChanged(IFsNode fsNode) =>
+            Changed?.Invoke(this, new FsNodeChangedEventArgs(fsNode));
+
+        public void Unsubscribe() =>
+            Changed = null;
+    }
+}
