6fac64f5020fbb5ffb1ac02da902f122be77b6ac
Romez <Romez1990@gmail.com>
2021-07-21 10:46:48 +0300

    feat(Utils.String): create string helper

---
+++ src/Utils/String/StringHelper.cs
@@ -0,0 +1,6 @@
+﻿namespace Utils.String {
+    public static class StringHelper {
+        public static string Capitalize(this string @string) =>
+            @string[0].ToString().ToUpper() + @string[1..];
+    }
+}
