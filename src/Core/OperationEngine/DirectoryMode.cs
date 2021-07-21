d675787340da825bf61e47195357664130ebf670
Romez <Romez1990@gmail.com>
2021-07-21 11:59:51 +0300

    feat(Core.OperationEngine): create directory mode

---
+++ src/Core/OperationEngine/DirectoryMode.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.OperationEngine {
+    public enum DirectoryMode {
+        Album,
+        Band,
+        Compilation,
+    }
+
+    public static class ModeExtension {
+        public static DirectoryMode Decrease(this DirectoryMode directoryMode) {
+            var modeNumber = (int)directoryMode;
+            var newNumber = modeNumber - 1;
+            return (DirectoryMode)newNumber;
+        }
+    }
+}
