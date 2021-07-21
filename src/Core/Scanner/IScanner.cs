78f54a262b09ec77a386f19daf98324171e36b23
Romez <Romez1990@gmail.com>
2021-07-21 12:00:23 +0300

    feat(Core.Scanner): create scanner

---
+++ src/Core/Scanner/IScanner.cs
@@ -0,0 +1,8 @@
+﻿using Core.FileSystemElement;
+using Core.OperationEngine;
+
+namespace Core.Scanner {
+    public interface IScanner {
+        IDirectoryElement Scan(IDirectoryElement directory, DirectoryMode directoryMode);
+    }
+}
