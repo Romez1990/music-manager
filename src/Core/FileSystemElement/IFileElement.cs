bce9ada8902e656073d6db29bd5d77471526c1ad
Romez <Romez1990@gmail.com>
2021-07-21 11:04:59 +0300

    feat(Core.FileSystemElement): create FileElement interface

---
+++ src/Core/FileSystemElement/IFileElement.cs
@@ -0,0 +1,12 @@
+using Core.FileSystem.Exceptions;
+using LanguageExt;
+
+namespace Core.FileSystemElement {
+    public interface IFileElement : IFsNodeElement {
+        string Extension { get; }
+        Either<FsException, IFileElement> Rename(string newName);
+        IFileElement Uncheck(bool ignoreIfUnchecked = false);
+        IFileElement Check(bool ignoreIfChecked = false);
+        IFileElement SetCheckState(CheckState checkState, bool ignoreIfSameState = false);
+    }
+}
