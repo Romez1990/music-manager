8036ef6de382f505bf8cad83828c5cab33a4c80b
Romez <Romez1990@gmail.com>
2021-07-21 13:20:34 +0300

    temp wpf

---
+++ src/WpfApp/FsNodeTemplateSelector.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Core.FileSystemElement;
+
+namespace WpfApp {
+    public class FsNodeTemplateSelector : DataTemplateSelector {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container) {
+            var templateName = ((IFsNodeElement)item).Match(
+                _ => "DirectoryTemplate",
+                _ => "DirectoryTemplate"
+            );
+            var element = (FrameworkElement)container;
+            return (DataTemplate)element.FindResource(templateName);
+        }
+    }
+}
---
+++ src/WpfApp/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Input;
+using Core.FileSystemElement;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace WpfApp {
+    public class MainViewModel : ViewModelBase {
+        public MainViewModel() {
+            DirectoryChecked = new RelayCommand(OnDirectoryChecked);
+
+            FsNodeElementFactory fsNodeElementFactory = null;
+            var directory = fsNodeElementFactory
+                .CreateDirectoryElement(@"D:\Dev projects\C#\music-manager-old\test\TestCompilation\Asking Alexandria")
+                .IfLeft(() => throw new Exception());
+            directory.RootChanged += OnDirectoryChanged;
+            SetDirectory(directory);
+        }
+
+        public ICommand DirectoryChecked { get; }
+
+        private void OnDirectoryChecked() {
+            Debug.Print("123");
+        }
+
+        private void OnDirectoryChanged(object sender, RootDirectoryElementChangedEventArgs e) =>
+            SetDirectory(e.Directory);
+
+        private IReadOnlyList<IFsNodeElement> _directory;
+
+        public IReadOnlyList<IFsNodeElement> Directory {
+            get => _directory;
+            private set => Set(() => Directory, ref _directory, value, true);
+        }
+
+        private void SetDirectory(IDirectoryElement directory) =>
+            Directory = new IFsNodeElement[] {directory};
+    }
+}
