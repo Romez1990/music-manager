c63e7aff5bd03cf8fc3390bb18ac537e3b1088f1
Romez <Romez1990@gmail.com>
2021-07-21 10:52:25 +0300

    feat(Core.FileSystem): create file interface

---
+++ src/Core/FileSystem/Exceptions/FsException.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace Core.FileSystem.Exceptions {
+    public abstract class FsException : Exception {
+        protected FsException(string message) : base(message) { }
+    }
+}
