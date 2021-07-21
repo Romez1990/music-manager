f22acb141a9401cdaf0b0db9c2f3787a37c3c899
Romez <Romez1990@gmail.com>
2021-07-21 10:46:48 +0300

    feat(Utils.String): create StringHelper

---
+++ test/Utils.Test/String/StringHelperTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using Utils.String;
+
+namespace Utils.Test.String {
+    [TestFixture]
+    public class StringHelperTest {
+        [TestCase("word", ExpectedResult = "Word")]
+        [TestCase("some word", ExpectedResult = "Some word")]
+        [TestCase("Capital", ExpectedResult = "Capital")]
+        [TestCase("Capital word", ExpectedResult = "Capital word")]
+        public string Capitalize(string @string) =>
+            @string.Capitalize();
+    }
+}
