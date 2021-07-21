fdc4665eac29639e651ac161b684fd61da64e9cf
Romez <Romez1990@gmail.com>
2021-07-21 11:04:29 +0300

    feat(Core.FileSystemElement): create FsNodeElement interface

---
+++ src/Core/FileSystemElement/FsNodeElementChangedEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Core.FileSystemElement {
+    public class FsNodeElementChangedEventArgs : EventArgs {
+        public FsNodeElementChangedEventArgs(IFsNodeElement fsNodeElement) {
+            FsNodeElement = fsNodeElement;
+        }
+
+        public IFsNodeElement FsNodeElement { get; }
+    }
+}
