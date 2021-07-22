feb11ddecccfc80c2250159a913b2fac8755aa3d
Romez <Romez1990@gmail.com>
2021-07-22 14:51:28 +0300

    feat(Utils.Reflection): add methods for method

--- src/Utils/Reflection/Exceptions/MemberNotFoundException.cs
+++ src/Utils/Reflection/Exceptions/MemberNotFoundException.cs
@@ -3,10 +3,16 @@ using Utils.String;
 
 namespace Utils.Reflection.Exceptions {
     public abstract class MemberNotFoundException : Exception {
-        protected MemberNotFoundException(string memberTypeName, Type type, string name)
-            : base(GetMessage(memberTypeName, type, name)) { }
+        protected MemberNotFoundException(string memberTypeName, Type type, string name, string postName = null)
+            : base(GetMessage(memberTypeName, type, name, postName)) { }
 
-        private static string GetMessage(string memberTypeName, Type type, string name) =>
-            $"{memberTypeName.Capitalize()} {name} not found in type {type}";
+        private static string GetMessage(string memberTypeName, Type type, string name, string postNameOrNull) =>
+            $"{memberTypeName.Capitalize()} {name}{GetPostName(postNameOrNull)} not found in type {type}";
+
+        private static string GetPostName(string postNameOrNull) =>
+            postNameOrNull switch {
+                null => "",
+                var postName => $" {postName}",
+            };
     }
 }
