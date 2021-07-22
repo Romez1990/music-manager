b6620a0fa4e21ff1f2aa5e8b25caab26c2e70be5
Romez <Romez1990@gmail.com>
2021-07-22 12:10:10 +0300

    feat(Utils.Enumerable): create EnumerableHelper

---
+++ src/Utils/Enumerable/EnumerableHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utils.Enumerable {
+    public static class EnumerableHelper {
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action) {
+            foreach (var element in source) {
+                action(element);
+            }
+        }
+    }
+}
