b7cca648a89030879153d538096f5e844dc465df
Romez <Romez1990@gmail.com>
2021-07-22 14:52:43 +0300

    feat(Utils.Reflection): add methods for constructor

---
+++ src/Utils/Reflection/Exceptions/ConstructionNotFountException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utils.Reflection.Exceptions {
+    public sealed class ConstructionNotFountException : MemberNotFoundException {
+        public ConstructionNotFountException(Type type, IEnumerable<Type> args)
+            : base("constructor", type, null, $"with types {GetArgTypes(args)}") { }
+
+        private static string GetArgTypes(IEnumerable<Type> argTypes) =>
+            string.Join(", ", argTypes);
+    }
+}
