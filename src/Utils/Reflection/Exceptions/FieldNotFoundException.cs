5ef0e9a4f607dc3de71f64ef7961d4052ad0be47
Romez <Romez1990@gmail.com>
2021-07-22 14:43:47 +0300

    feat(Utils.Reflection): add methods for field

---
+++ src/Utils/Reflection/Exceptions/FieldNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Utils.Reflection.Exceptions {
+    public sealed class FieldNotFoundException : MemberNotFoundException {
+        public FieldNotFoundException(Type type, string name)
+            : base("field", type, name) { }
+    }
+}
--- src/Utils/Reflection/ReflectionHelper.cs
+++ src/Utils/Reflection/ReflectionHelper.cs
@@ -21,5 +21,21 @@ namespace Utils.Reflection {
 
         public static void SetPropertyValue(this object @object, string propertyName, object value) =>
             @object.GetType().GetPropertyInfo(propertyName).SetValue(@object, value);
+
+        public static FieldInfo GetFieldInfo(this Type type, string fieldName) {
+            var fieldInfo = type.GetField(fieldName, DefaultLookup);
+            if (fieldInfo is null)
+                throw new FieldNotFoundException(type, fieldName);
+            return fieldInfo;
+        }
+
+        public static T GetValue<T>(this FieldInfo fieldInfo, object @object) =>
+            (T)fieldInfo.GetValue(@object);
+
+        public static T GetFieldValue<T>(this object @object, string fieldName) =>
+            @object.GetType().GetFieldInfo(fieldName).GetValue<T>(@object);
+
+        public static void SetFieldValue(this object @object, string fieldName, object value) =>
+            @object.GetType().GetFieldInfo(fieldName).SetValue(@object, value);
     }
 }
