60bf0c442af86cf720cba934d8def51a2e3b54a5
Romez <Romez1990@gmail.com>
2021-07-24 11:09:26 +0300

    feat(Core.FileSystem): add CreateDirectory and CreateFile methods to FsNodeFactory

--- src/Core/FileSystem/IFsNodeFactory.cs
+++ src/Core/FileSystem/IFsNodeFactory.cs
@@ -1,7 +1,12 @@
-using System.IO;
+using Core.FileSystem.Exceptions;
+using LanguageExt;
+using DirectoryInfo = System.IO.DirectoryInfo;
+using FileInfo = System.IO.FileInfo;
 
 namespace Core.FileSystem {
     public interface IFsNodeFactory {
+        Either<DirectoryNotFoundException, IDirectory> CreateDirectory(string path);
+        Either<FileNotFoundException, IFile> CreateFile(string path);
         IDirectory CreateDirectoryFromInfo(DirectoryInfo info, ChildrenRetrieval<IFsNode> childrenRetrieval);
         IFile CreateFileFromInfo(FileInfo info);
     }
