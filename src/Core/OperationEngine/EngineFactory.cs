27d9d0959d120d4e8020caa08f83376db846898e
Romez <Romez1990@gmail.com>
2021-07-24 13:05:06 +0300

    feat(Core.OperationEngine): create EnginePerformer

--- src/Core/OperationEngine/EngineFactory.cs
+++ src/Core/OperationEngine/EngineFactory.cs
@@ -1,20 +1,23 @@
 ﻿using Core.Scanner;
 using Core.FileSystemElement;
 using Core.IocContainer;
+using Core.Operation;
 
 namespace Core.OperationEngine {
     [Service]
     public class EngineFactory : IEngineFactory {
-        public EngineFactory(IScanner scanner) {
+        public EngineFactory(IScanner scanner, IOperationRepository operations) {
             _scanner = scanner;
+            _operations = operations;
         }
 
         private readonly IScanner _scanner;
+        private readonly IOperationRepository _operations;
 
         public IEngineScanner CreateScanner(IDirectoryElement directory) =>
             new EngineScanner(this, _scanner, directory);
 
         public IEnginePerformer CreateEnginePerformer(IDirectoryElement directory, DirectoryMode directoryMode) =>
-            throw new System.NotImplementedException();
+            new EnginePerformer(_operations, directory, directoryMode);
     }
 }
