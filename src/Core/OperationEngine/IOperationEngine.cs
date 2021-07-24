93090f9977e3b146d297672da6725306b33fd0c0
Romez <Romez1990@gmail.com>
2021-07-24 10:24:12 +0300

    feat(Core.OperationEngine): create engine

---
+++ src/Core/OperationEngine/IOperationEngine.cs
@@ -0,0 +1,8 @@
+﻿using Core.FileSystem.Exceptions;
+using LanguageExt;
+
+namespace Core.OperationEngine {
+    public interface IOperationEngine {
+        Either<DirectoryNotFoundException, IEngineScanner> SetDirectory(string path);
+    }
+}
---
+++ src/Core/OperationEngine/OperationEngine.cs
@@ -0,0 +1,21 @@
+﻿using Core.FileSystem.Exceptions;
+using Core.FileSystemElement;
+using Core.IocContainer;
+using LanguageExt;
+
+namespace Core.OperationEngine {
+    [Service]
+    public class OperationEngine : IOperationEngine {
+        public OperationEngine(IEngineFactory engineFactory, IFsNodeElementFactory fsNodeElementFactory) {
+            _engineFactory = engineFactory;
+            _fsNodeElementFactory = fsNodeElementFactory;
+        }
+
+        private readonly IEngineFactory _engineFactory;
+        private readonly IFsNodeElementFactory _fsNodeElementFactory;
+
+        public Either<DirectoryNotFoundException, IEngineScanner> SetDirectory(string path) =>
+            _fsNodeElementFactory.CreateDirectoryElement(path)
+                .Map(directory => _engineFactory.CreateScanner(directory));
+    }
+}
