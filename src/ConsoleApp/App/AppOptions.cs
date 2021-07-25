e130a6f2d22a4a344b7ef0dda3bcd9b9a8448192
Romez <Romez1990@gmail.com>
2021-07-25 09:00:25 +0300

    feat(ConsoleApp.App): create AppOptions

---
+++ src/ConsoleApp/App/AppOptions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Core.OperationEngine;
+
+namespace ConsoleApp.App {
+    public class AppOptions : IAppOptions {
+        public AppOptions(string path, DirectoryMode directoryMode, IReadOnlyList<string> operations) {
+            Path = path;
+            DirectoryMode = directoryMode;
+            Operations = operations;
+        }
+
+        public string Path { get; }
+        public DirectoryMode DirectoryMode { get; }
+        public IReadOnlyList<string> Operations { get; }
+    }
+}
---
+++ src/ConsoleApp/App/IAppOptions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Core.OperationEngine;
+
+namespace ConsoleApp.App {
+    public interface IAppOptions {
+        string Path { get; }
+        DirectoryMode DirectoryMode { get; }
+        IReadOnlyList<string> Operations { get; }
+    }
+}
