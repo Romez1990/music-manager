06c9c1dec2a48d1d3a1d22f4896fe5acc65d1264
Romez <Romez1990@gmail.com>
2021-07-25 12:57:15 +0300

    feat(Core.IocContainer): create ServiceAttribute

---
+++ src/Core/IocContainer/ServiceAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Core.IocContainer {
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ServiceAttribute : Attribute {
+        public bool ToSelf { get; init; }
+    }
+}
