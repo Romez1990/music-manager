610d07426cc4df9b4e9d1492e4a9534229e2751a
Romez <Romez1990@gmail.com>
2021-07-25 09:04:40 +0300

    feat(ConsoleApp.ArgumentParser): create OptionsBase

---
+++ src/ConsoleApp/ArgumentParser/OptionsBase.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+
+namespace ConsoleApp.ArgumentParser {
+    public class OptionsBase {
+        [Value(0, HelpText = "Directory path")]
+        public string Path { get; set; }
+
+        [Option('a', "album", SetName = "album", HelpText = "Album directory mode")]
+        public bool Album { get; set; }
+
+        [Option('b', "band", SetName = "band", HelpText = "Band directory mode")]
+        public bool Band { get; set; }
+
+        [Option('c', "compilation", SetName = "compilation", HelpText = "Compilation directory mode")]
+        public bool Compilation { get; set; }
+    }
+}
