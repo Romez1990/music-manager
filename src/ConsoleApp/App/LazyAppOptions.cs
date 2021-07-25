8fa2fbc64f65dfd8aee4d05ca8a9636c4b918904
Romez <Romez1990@gmail.com>
2021-07-25 09:00:40 +0300

    feat(ConsoleApp.App): create LazyAppOptions

---
+++ src/ConsoleApp/App/LazyAppOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.OperationEngine;
+
+namespace ConsoleApp.App {
+    public class LazyAppOptions : IAppOptions {
+        public LazyAppOptions(Func<string> getPath, DirectoryMode directoryMode,
+            Func<IReadOnlyList<string>> getOperations) {
+            _path = new Lazy<string>(getPath);
+            DirectoryMode = directoryMode;
+            _operations = new Lazy<IReadOnlyList<string>>(getOperations);
+        }
+
+        private readonly Lazy<string> _path;
+        private readonly Lazy<IReadOnlyList<string>> _operations;
+
+        public string Path => _path.Value;
+        public DirectoryMode DirectoryMode { get; }
+        public IReadOnlyList<string> Operations => _operations.Value;
+    }
+}
