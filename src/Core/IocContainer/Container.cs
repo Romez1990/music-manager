510a4b427ff259a929b46438d14abda0a8c8e403
Romez <Romez1990@gmail.com>
2021-07-26 22:48:14 +0300

    feat(Core.IocContainer): add scan assemblies method

--- src/Core/IocContainer/Container.cs
+++ src/Core/IocContainer/Container.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Autofac;
+using Utils.Enumerable;
 using IAutofacContainer = Autofac.IContainer;
 
 namespace Core.IocContainer {
@@ -11,6 +15,43 @@ namespace Core.IocContainer {
 
         private readonly ContainerBuilder _containerBuilder;
         private readonly Lazy<IAutofacContainer> _container;
+
+        public void ScanAssemblies(params string[] assemblyNames) =>
+            RegisterAssemblyTypes(LoadAssemblies(assemblyNames));
+
+        public void ScanAssemblies(IEnumerable<string> assemblyNames) =>
+            RegisterAssemblyTypes(LoadAssemblies(assemblyNames));
+
+        public void ScanAssemblies(params Assembly[] assemblies) =>
+            RegisterAssemblyTypes(assemblies);
+
+        public void ScanAssemblies(IEnumerable<Assembly> assemblies) =>
+            RegisterAssemblyTypes(assemblies);
+
+        private IEnumerable<Assembly> LoadAssemblies(IEnumerable<string> assemblyNames) =>
+            assemblyNames.Map(Assembly.Load);
+
+        private void RegisterAssemblyTypes(IEnumerable<Assembly> assemblies) =>
+            assemblies.ForEach(assembly =>
+                _containerBuilder.RegisterAssemblyTypes(assembly)
+                    .Where(IsService)
+                    .As(GetAs));
+
+        private bool IsService(Type type) =>
+            type.IsDefined(typeof(ServiceAttribute), false);
+
+        private Type GetAs(Type type) {
+            var attributes = type.GetCustomAttributes(typeof(ServiceAttribute), false);
+            if (attributes.Length > 1)
+                throw new Exception("Service attribute must be defined only once");
+            var attribute = (ServiceAttribute)attributes.First();
+            return attribute.ToSelf
+                ? type
+                : GetInterface(type);
+        }
+
+        private Type GetInterface(Type type) =>
+            type.GetInterfaces().First();
 
         public T Get<T>() where T : class =>
             _container.Value.Resolve<T>();
--- src/Core/IocContainer/IContainer.cs
+++ src/Core/IocContainer/IContainer.cs
@@ -1,5 +1,12 @@
-﻿namespace Core.IocContainer {
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Core.IocContainer {
     public interface IContainer {
+        void ScanAssemblies(params string[] assemblyNames);
+        void ScanAssemblies(IEnumerable<string> assemblyNames);
+        void ScanAssemblies(params Assembly[] assemblies);
+        void ScanAssemblies(IEnumerable<Assembly> assemblies);
         T Get<T>() where T : class;
     }
 }
