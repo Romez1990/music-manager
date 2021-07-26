e6bb1c4b81a130c3965c2a6a8809b344bbfbcf0c
Romez <Romez1990@gmail.com>
2021-07-26 22:39:09 +0300

    feat(Core.IocContainer): create Container

---
+++ src/Core/IocContainer/Container.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Autofac;
+using IAutofacContainer = Autofac.IContainer;
+
+namespace Core.IocContainer {
+    public class Container : IContainer {
+        public Container() {
+            _containerBuilder = new ContainerBuilder();
+            _container = new Lazy<IAutofacContainer>(() => _containerBuilder.Build());
+        }
+
+        private readonly ContainerBuilder _containerBuilder;
+        private readonly Lazy<IAutofacContainer> _container;
+    }
+}
