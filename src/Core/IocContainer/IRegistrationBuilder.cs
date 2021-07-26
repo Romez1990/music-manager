d605f436e60583374e1e802d82909613a4cf32a2
Romez <Romez1990@gmail.com>
2021-07-26 22:50:37 +0300

    feat(Core.IocContainer): add register method

---
+++ src/Core/IocContainer/IRegistrationBuilder.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Core.IocContainer {
+    public interface IRegistrationBuilder {
+        void As<T>() where T : class;
+        void As(Type type);
+        void AsSelf();
+    }
+}
