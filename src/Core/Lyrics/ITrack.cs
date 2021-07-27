cd350494cd8a5219e8df4c8ffe600f1e89b56c96
Romez <Romez1990@gmail.com>
2021-07-27 16:52:59 +0300

    feat(Core.Lyrics): create track

---
+++ src/Core/Lyrics/ITrack.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using LanguageExt;
+
+namespace Core.Lyrics {
+    public interface ITrack {
+        Option<IEnumerable<string>> Artists { get; }
+        Option<string> Title { get; }
+        string Lyrics { get; set; }
+        void Save();
+    }
+}
---
+++ src/Core/Lyrics/Track.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core.FileSystemElement;
+using LanguageExt;
+using TagLib;
+using static LanguageExt.Prelude;
+
+namespace Core.Lyrics {
+    public class Track : ITrack {
+        public Track(IFileElement file) {
+            _file = File.Create(file.Path);
+        }
+
+        private readonly File _file;
+
+        public Option<IEnumerable<string>> Artists =>
+            Optional(_file.Tag.AlbumArtists)
+                .Map(albumArtists => (IEnumerable<string>)albumArtists);
+
+        public Option<string> Title => _file.Tag.Title;
+
+        public string Lyrics {
+            get => _file.Tag.Lyrics;
+            set => _file.Tag.Lyrics = value;
+        }
+
+        public void Save() =>
+            _file.Save();
+    }
+}
