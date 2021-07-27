f8359c80b8bb3b6041db727a71a6bbc22ddad2e1
Romez <Romez1990@gmail.com>
2021-07-27 13:52:24 +0300

    feat(ConsoleApp): call ScanAssemblies

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,3 +1,8 @@
 ﻿using System;
+using System.Reflection;
+using Core.IocContainer;
+
+var container = new Container();
+container.ScanAssemblies(Assembly.GetExecutingAssembly().FullName, "Core");
 
 Console.WriteLine("Hello, world!");
