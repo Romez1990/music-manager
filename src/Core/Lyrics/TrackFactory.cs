ec16cd0342cba66a52ab8cb0be5545c90fc455ba
Romez <Romez1990@gmail.com>
2021-07-27 16:53:05 +0300

    feat(Core.Lyrics): create track factory

---
+++ src/Core/Lyrics/TrackFactory.cs
@@ -0,0 +1,10 @@
+﻿using Core.FileSystemElement;
+using Core.IocContainer;
+
+namespace Core.Lyrics {
+    [Service]
+    public class TrackFactory : ITrackFactory {
+        public ITrack CreateTrack(IFileElement file) =>
+            new Track(file);
+    }
+}
