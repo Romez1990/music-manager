4d32b7a9b64a349e174edef5c9d1db429017e705
Romez <Romez1990@gmail.com>
2021-07-27 13:27:09 +0300

    feat(Core.Serializers): create yaml serializer

---
+++ src/Core/Serializers/YamlSerializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace Core.Serializers {
+    public class YamlSerializer : ISerializer {
+        public YamlSerializer(NamingConvention namingConvention) {
+            var convention = GetNamingConvention(namingConvention);
+            _serializer = new Lazy<YamlDotNet.Serialization.ISerializer>(() => new SerializerBuilder()
+                .WithNamingConvention(convention)
+                .Build());
+            _deserializer = new Lazy<IDeserializer>(() => new DeserializerBuilder()
+                .WithNamingConvention(convention)
+                .Build());
+        }
+
+        public string FileExtension => ".yml";
+
+        private readonly Lazy<YamlDotNet.Serialization.ISerializer> _serializer;
+        private readonly Lazy<IDeserializer> _deserializer;
+
+        private INamingConvention GetNamingConvention(NamingConvention namingConvention) =>
+            namingConvention switch {
+                NamingConvention.SnakeCase => UnderscoredNamingConvention.Instance,
+                NamingConvention.CamelCase => CamelCaseNamingConvention.Instance,
+                NamingConvention.PascalCase => PascalCaseNamingConvention.Instance,
+                NamingConvention.KebabCase => HyphenatedNamingConvention.Instance,
+                _ => throw new NotSupportedException(),
+            };
+
+        public string Serialize<T>(T value) =>
+            _serializer.Value.Serialize(value);
+
+        public T Deserialize<T>(string input) =>
+            _deserializer.Value.Deserialize<T>(input);
+    }
+}
