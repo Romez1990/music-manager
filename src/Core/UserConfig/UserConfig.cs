6f4110a40eebd293722b6c788d0a96c82499debe
Romez <Romez1990@gmail.com>
2021-07-28 10:03:29 +0300

    feat(Core.UserConfig): add Equals method to user config

--- src/Core/UserConfig/UserConfig.cs
+++ src/Core/UserConfig/UserConfig.cs
@@ -1,5 +1,19 @@
-﻿namespace Core.UserConfig {
+﻿using System.Linq;
+
+namespace Core.UserConfig {
     public class UserConfig {
         public string GeniusApiToken { get; set; }
+
+        public bool Equals(UserConfig that) =>
+            GetType()
+                .GetProperties()
+                .All(property => {
+                    var thisValue = property.GetValue(this);
+                    var thatValue = property.GetValue(that);
+                    if (thisValue is null) {
+                        return thatValue is null;
+                    }
+                    return thisValue.Equals(thatValue);
+                });
     }
 }
