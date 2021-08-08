1ca757150cf751fd674c0bfbcc8f0b8a5af4dd11
Romez <Romez1990@gmail.com>
2021-08-08 10:48:15 +0300

    feat(Core.FileSystem): add create directory from info method to fs node factory

--- src/Core/FileSystem/IFsNodeFactory.cs
+++ src/Core/FileSystem/IFsNodeFactory.cs
@@ -2,6 +2,7 @@ using System.IO;
 
 namespace Core.FileSystem {
     public interface IFsNodeFactory {
+        IDirectory CreateDirectoryFromInfo(DirectoryInfo info, ChildrenRetrieval<IFsNode> childrenRetrieval);
         IFile CreateFileFromInfo(FileInfo info);
     }
 }
