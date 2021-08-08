e573c5d80c46d175842950c917d9e8325cde91f5
Romez <Romez1990@gmail.com>
2021-08-08 18:09:07 +0300

    feat(Core.FileSystem): implement CreateDirectoryFromInfo method in FsNodeFactory

--- src/Core/FileSystem/FsNodeFactory.cs
+++ src/Core/FileSystem/FsNodeFactory.cs
@@ -1,11 +1,18 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using Core.IocContainer;
 
 namespace Core.FileSystem {
     [Service]
     public class FsNodeFactory : IFsNodeFactory {
+        public FsNodeFactory(INaturalStringComparerFactory naturalStringComparerFactory) {
+            _naturalStringComparer = naturalStringComparerFactory.Create();
+        }
+
+        private readonly IComparer<string> _naturalStringComparer;
+
         public IDirectory CreateDirectoryFromInfo(DirectoryInfo info, ChildrenRetrieval<IFsNode> childrenRetrieval) =>
-            throw new System.NotImplementedException();
+            new Directory(this, _naturalStringComparer, info, childrenRetrieval);
 
         public IFile CreateFileFromInfo(FileInfo info) =>
             new File(this, info);
