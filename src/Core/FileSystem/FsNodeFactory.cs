e39f1facf5c630bffc0eeddf49c116492efed5a0
Romez <Romez1990@gmail.com>
2021-08-08 10:48:15 +0300

    feat(Core.FileSystem): add CreateDirectoryFromInfo method to FsNodeFactory

--- src/Core/FileSystem/FsNodeFactory.cs
+++ src/Core/FileSystem/FsNodeFactory.cs
@@ -4,6 +4,9 @@ using Core.IocContainer;
 namespace Core.FileSystem {
     [Service]
     public class FsNodeFactory : IFsNodeFactory {
+        public IDirectory CreateDirectoryFromInfo(DirectoryInfo info, ChildrenRetrieval<IFsNode> childrenRetrieval) =>
+            throw new System.NotImplementedException();
+
         public IFile CreateFileFromInfo(FileInfo info) =>
             new File(this, info);
     }
