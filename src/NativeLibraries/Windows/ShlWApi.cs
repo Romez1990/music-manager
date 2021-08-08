af9bbc437a1695770ecb0a75ee25e14ee2857a08
Romez <Romez1990@gmail.com>
2021-08-08 16:16:23 +0300

    feat(NativeLibraries.Windows): create ShlWApi library

---
+++ src/NativeLibraries/Windows/ShlWApi.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.InteropServices;
+
+namespace NativeLibraries.Windows {
+    public static class ShlWApi {
+        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
+        public static extern int StrCmpLogicalW(string psz1, string psz2);
+    }
+}
