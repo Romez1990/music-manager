1ce8b573bfd9189f226507bd49a23b5d2b52e7b0
Romez <Romez1990@gmail.com>
2021-08-08 10:55:29 +0300

    feat(Core.FileSystem): create windows natural string comparer

---
+++ src/Core/FileSystem/WindowsNaturalStringComparer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using NativeLibraries.Windows;
+
+namespace Core.FileSystem {
+    public class WindowsNaturalStringComparer : IComparer<string> {
+        public int Compare(string a, string b) =>
+            ShlWApi.StrCmpLogicalW(a, b);
+    }
+}
