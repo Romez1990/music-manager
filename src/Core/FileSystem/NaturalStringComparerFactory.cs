dc4261feb0b376eb15a35b2264350ba255e34650
Romez <Romez1990@gmail.com>
2021-08-08 18:15:50 +0300

    feat(Core.FileSystem): create natural string compare factory

---
+++ src/Core/FileSystem/NaturalStringComparerFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.IocContainer;
+
+namespace Core.FileSystem {
+    [Service]
+    public class NaturalStringComparerFactory : INaturalStringComparerFactory {
+        public IComparer<string> Create() {
+            if (OperatingSystem.IsWindows())
+                return new WindowsNaturalStringComparer();
+            throw new PlatformNotSupportedException();
+        }
+    }
+}
