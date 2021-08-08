e7495e22059b12fab421228b6dad61bb6c2ef1ca
Romez <Romez1990@gmail.com>
2021-08-08 20:36:21 +0300

    feat(Core.FileElement): create FsNodeElementFactory

---
+++ src/Core/FileSystemElement/FsNodeElementFactory.cs
@@ -0,0 +1,13 @@
+﻿using Core.FileSystem;
+using Core.IocContainer;
+
+namespace Core.FileSystemElement {
+    [Service]
+    public class FsNodeElementFactory : IFsNodeElementFactory {
+        public IFileElement CreateFileElementFromFile(IFile file) =>
+            throw new System.NotImplementedException();
+
+        public IFileElement CreateFileElementFromFile(IFile file, CheckState checkState) =>
+            throw new System.NotImplementedException();
+    }
+}
