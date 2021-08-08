e238fde9402c8a28c2143e505dc76254d4cdc131
Romez <Romez1990@gmail.com>
2021-08-08 20:54:38 +0300

    feat(Core.FileSystemElement): add CreateDirectoryElementFromDirectory method to FsNodeElementFactory

--- src/Core/FileSystemElement/IFsNodeElementFactory.cs
+++ src/Core/FileSystemElement/IFsNodeElementFactory.cs
@@ -2,6 +2,11 @@
 
 namespace Core.FileSystemElement {
     public interface IFsNodeElementFactory {
+        IDirectoryElement CreateDirectoryElementFromDirectory(IDirectory directory);
+
+        IDirectoryElement CreateDirectoryElementFromDirectory(IDirectory directory, CheckState checkState,
+            bool isExpanded, ChildrenRetrieval<IFsNodeElement> childrenRetrieval);
+
         IFileElement CreateFileElementFromFile(IFile file);
         IFileElement CreateFileElementFromFile(IFile file, CheckState checkState);
     }
