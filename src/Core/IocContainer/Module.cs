8c9bd82032cf65abd7c0f57cdd1617fddad00e33
Romez <Romez1990@gmail.com>
2021-08-09 11:04:32 +0300

    feat(Core.IocContainer): create Module

---
+++ src/Core/IocContainer/Module.cs
@@ -0,0 +1,22 @@
+﻿namespace Core.IocContainer {
+    public abstract class Module : IModule {
+        public void Load(Container container) {
+            _container = container;
+            Load();
+        }
+
+        private Container _container;
+
+        protected abstract void Load();
+
+        protected RegistrationBuilder Register<T>() where T : class =>
+            new((toSelf, serviceInterface) => {
+                var registrationBuilder = _container.Register<T>();
+                if (toSelf) {
+                    registrationBuilder.AsSelf();
+                } else {
+                    registrationBuilder.As(serviceInterface);
+                }
+            });
+    }
+}
