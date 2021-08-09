6d6e692aad2f8616a31def1cd93b23731d1b6afb
Romez <Romez1990@gmail.com>
2021-08-09 10:13:09 +0300

    feat(Core.FileSystemElement): implement create directory element from directory method in fs node element factory

--- src/Core/FileSystemElement/FsNodeElementFactory.cs
+++ src/Core/FileSystemElement/FsNodeElementFactory.cs
@@ -5,13 +5,15 @@ namespace Core.FileSystemElement {
     [Service]
     public class FsNodeElementFactory : IFsNodeElementFactory {
         private const CheckState StartingCheckState = CheckState.Unchecked;
+        private const bool StartingIsExpanded = false;
 
         public IDirectoryElement CreateDirectoryElementFromDirectory(IDirectory directory) =>
-            throw new System.NotImplementedException();
+            CreateDirectoryElementFromDirectory(directory, StartingCheckState, StartingIsExpanded,
+                ChildrenRetrieval<IFsNodeElement>.Create());
 
         public IDirectoryElement CreateDirectoryElementFromDirectory(IDirectory directory, CheckState checkState,
             bool isExpanded, ChildrenRetrieval<IFsNodeElement> childrenRetrieval) =>
-            throw new System.NotImplementedException();
+            new DirectoryElement(this, directory, checkState, isExpanded, childrenRetrieval);
 
         public IFileElement CreateFileElementFromFile(IFile file) =>
             CreateFileElementFromFile(file, StartingCheckState);
