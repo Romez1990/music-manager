9976c8a49dec82b5e412c9311bab38ed2b99b285
Romez <Romez1990@gmail.com>
2021-08-10 23:10:11 +0300

    temp feat(Core.TagFiller): create TagFiller

---
+++ src/Core/TagFiller/TagFiller.cs
@@ -0,0 +1,23 @@
+﻿using Core.FileSystemElement;
+using Core.NamingFormats;
+using Core.OperationEngine;
+
+namespace Core.TagFiller {
+    public class TagFiller {
+        public TagFiller(IAlbumNaming albumNaming, ITrackNaming trackNaming) {
+            _albumNaming = albumNaming;
+            _trackNaming = trackNaming;
+        }
+
+        private readonly IAlbumNaming _albumNaming;
+        private readonly ITrackNaming _trackNaming;
+
+        public void FillTags(IDirectoryElement directory, DirectoryMode directoryMode) {
+
+        }
+
+        private void FillTagsForAlbum(IDirectoryElement albumDirectory) {
+
+        }
+    }
+}
