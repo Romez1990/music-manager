6ec46ca91944efe23bc8f7d6b65d3dafda8ebeda
Romez <Romez1990@gmail.com>
2022-08-01 14:50:13 +0300

    feat(Core.Operation): create LyricsOperation and add it to the repository

---
+++ src/Core/Operation/LyricsOperation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Immutable;
+using Core.FileSystemElement;
+using Core.IocContainer;
+using Core.Lyrics;
+using Core.OperationEngine;
+
+namespace Core.Operation {
+    [Service(ToSelf = true)]
+    public class LyricsOperation : IOperation {
+        public LyricsOperation(ILyricsService lyricsService) {
+            _lyricsService = lyricsService;
+        }
+
+        private readonly ILyricsService _lyricsService;
+
+        public string Name => "Lyrics";
+
+        public string Description => "Find lyrics for songs and write it to file";
+
+        public OperationResult Perform(IDirectoryElement directory, DirectoryMode directoryMode) {
+            var task = _lyricsService.AddLyrics(directory, directoryMode);
+            task.Wait();
+            var exceptions = task.Result
+                .Map(fsException => new OperationException(fsException))
+                .ToImmutableList();
+            return new OperationResult(directory, exceptions);
+        }
+    }
+}
