ab27f783e962cf871a9280e14ed5c173e30f6f96
Romez <Romez1990@gmail.com>
2022-08-01 14:49:50 +0300

    feat(Core.Lyrics): create LyricsService

---
+++ src/Core/Lyrics/ILyricsService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.FileSystemElement;
+using Core.OperationEngine;
+
+namespace Core.Lyrics {
+    public interface ILyricsService {
+        Task<IReadOnlyList<LyricsException>> AddLyrics(IDirectoryElement directory,
+            DirectoryMode directoryMode);
+    }
+}
