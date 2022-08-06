ef30ad5baef05fcbdfcf3fc46c35ac855f10b2a2
Romez <Romez1990@gmail.com>
2022-08-06 17:31:56 +0300

    feat(Core.Operation): create RenameOperation

---
+++ src/Core/Operation/RenameOperation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Immutable;
+using Core.FileSystemElement;
+using Core.IocContainer;
+using Core.OperationEngine;
+using Core.Renamer;
+
+namespace Core.Operation {
+    [Service(ToSelf = true)]
+    public class RenameOperation : IOperation {
+        public RenameOperation(IRenamer renamer) {
+            _renamer = renamer;
+        }
+
+        public string Name => "Rename";
+
+        public string Description => "Rename track files and album folders";
+
+        private readonly IRenamer _renamer;
+
+        public OperationResult Perform(IDirectoryElement directory, DirectoryMode directoryMode) {
+            var renamingResult = _renamer.Rename(directory, directoryMode);
+            return new OperationResult(renamingResult.Directory, renamingResult.Exceptions
+                .Map(fsException => new OperationException(fsException))
+                .ToImmutableList());
+        }
+    }
+}
