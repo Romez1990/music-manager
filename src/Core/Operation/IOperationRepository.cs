7fc7fcce834621fde5b0c31e87a16c9767f3774e
Romez <Romez1990@gmail.com>
2022-08-06 17:36:42 +0300

    feat(Core.Operation): create operation repository

---
+++ src/Core/Operation/IOperationRepository.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Operation {
+    public interface IOperationRepository {
+        IEnumerable<IOperation> FindAll();
+        IEnumerable<IOperation> FindByNames(IEnumerable<string> names);
+    }
+}
