76c6946ef4df9621f27eb8c9864883ee36773b19
Romez <Romez1990@gmail.com>
2022-08-06 17:29:59 +0300

    feat(Core.NamingFormats): create TrackNaming

---
+++ src/Core/NamingFormats/ITrackNaming.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using LanguageExt;
+
+namespace Core.NamingFormats {
+    public interface ITrackNaming {
+        int FindPrefixLength(IEnumerable<string> trackNames);
+        Option<string> Normalize(string trackName, int trackNumber, int trackNumberLength, int prefixLength);
+    }
+}
