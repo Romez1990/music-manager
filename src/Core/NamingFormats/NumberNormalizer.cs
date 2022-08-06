b10b1f1ef0d765dec6b238ec6f04f580e1a282bc
Romez <Romez1990@gmail.com>
2022-08-06 17:29:32 +0300

    feat(Core.NamingFormats): create NumberNormalizer

---
+++ src/Core/NamingFormats/NumberNormalizer.cs
@@ -0,0 +1,9 @@
+﻿using Core.IocContainer;
+
+namespace Core.NamingFormats {
+    [Service]
+    public class NumberNormalizer : INumberNormalizer {
+        public string Normalize(int number, int numberLength) =>
+            number.ToString().PadLeft(numberLength, '0');
+    }
+}
