fc7818f9e18c53544b0670bdd5fc53a4894b0f93
Romez <Romez1990@gmail.com>
2022-08-06 17:31:38 +0300

    feat(Core.Renamer): create Renamer

---
+++ src/Core/Renamer/IRenamer.cs
@@ -0,0 +1,8 @@
+﻿using Core.FileSystemElement;
+using Core.OperationEngine;
+
+namespace Core.Renamer {
+    public interface IRenamer {
+        RenamingResult Rename(IDirectoryElement directory, DirectoryMode mode);
+    }
+}
