be5c7323072dfbb6dd668dcbe359c913c22dfabd
Romez <Romez1990@gmail.com>
2022-08-06 13:33:42 +0300

    feat(Core.NamingFormats): create RegexMatcher

---
+++ src/Core/NamingFormats/IRegexMatcher.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using LanguageExt;
+
+namespace Core.NamingFormats {
+    public interface IRegexMatcher {
+        Option<string> Rename(Regex regex, string input, Func<Match, string> onMatch);
+    }
+}
---
+++ src/Core/NamingFormats/RegexMatcher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Core.IocContainer;
+using LanguageExt;
+using static LanguageExt.Prelude;
+
+namespace Core.NamingFormats {
+    [Service]
+    public class RegexMatcher : IRegexMatcher {
+        public Option<string> Rename(Regex regex, string input, Func<Match, string> onMatch) {
+            var match = regex.Match(input);
+            if (!match.Success)
+                return None;
+            var result = onMatch(match);
+            return CheckIfNotChanged(input, result);
+        }
+
+        private Option<string> CheckIfNotChanged(string input, string result) =>
+            result == input
+                ? None
+                : result;
+    }
+}
