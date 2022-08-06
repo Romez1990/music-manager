68d33eacaec5ecc553b8f77574b5b831e472ba38
Romez <Romez1990@gmail.com>
2022-08-06 17:36:42 +0300

    feat(Core.Operation): create OperationRepository

---
+++ src/Core/Operation/OperationRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core.IocContainer;
+
+namespace Core.Operation {
+    [Service]
+    public class OperationRepository : IOperationRepository {
+        public OperationRepository(
+            RenameOperation rename
+        ) {
+            _operations = new IOperation[] {
+                rename,
+            };
+        }
+
+        private readonly IReadOnlyList<IOperation> _operations;
+
+        public IEnumerable<IOperation> FindAll() =>
+            _operations;
+
+        public IEnumerable<IOperation> FindByNames(IEnumerable<string> names) =>
+            _operations.Where(operation => names.Contains(operation.Name))
+                .ToArray();
+    }
+}
